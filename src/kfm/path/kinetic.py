bebd3e6ccb15049475aa3543d568f6b32d53d9f0
Xander Baatz <35949906+XanderBaatz@users.noreply.github.com>
2026-09-14 14:04:07 +0000

    default to cubic kinetic path, and moved zero_cog to after target construction like tdm

--- src/kfm/path/kinetic.py
+++ src/kfm/path/kinetic.py
@@ -40,7 +40,7 @@ class KineticCubicProbPath(ProbPath):
         d = self.manifold.logmap(x_0, x_1)
 
         if self.zero_cog_v and node_index is not None:
-            d = scatter_center(d, index=node_index)
+            # d = scatter_center(d, index=node_index)
             v_0 = scatter_center(v_0, index=node_index)
             v_1 = scatter_center(v_1, index=node_index)
 
@@ -51,10 +51,17 @@ class KineticCubicProbPath(ProbPath):
             omega_t = (3 * t2 - 2 * t3) * d
             v_t = (6 * t_exp - 6 * t2) * d
             u_t_v = (6.0 - 12.0 * t_exp) * d
+            dx_t = d
         else:
             omega_t = (3 * t2 - 2 * t3) * d + (t_exp - 2 * t2 + t3) * v_0 + (-t2 + t3) * v_1
             v_t = (6 * t_exp - 6 * t2) * d + (1 - 4 * t_exp + 3 * t2) * v_0 + (-2 * t_exp + 3 * t2) * v_1
             u_t_v = (6 - 12 * t_exp) * d + (-4 + 6 * t_exp) * v_0 + (-2 + 6 * t_exp) * v_1
+            dx_t = u_t_v
+
+        if self.zero_cog_v and node_index is not None:
+            v_t = scatter_center(v_t, index=node_index)
+            u_t_v = scatter_center(u_t_v, index=node_index)
+            dx_t = scatter_center(dx_t, index=node_index)
 
         x_t = self.manifold.expmap(x=x_0, u=omega_t)
 
@@ -65,7 +72,7 @@ class KineticCubicProbPath(ProbPath):
             v_1=v_1,
             x_t=x_t,
             v_t=v_t,
-            dx_t=d if self.simplified else u_t_v,
+            dx_t=dx_t,
             dv_t=u_t_v,
             t=t,
         )
